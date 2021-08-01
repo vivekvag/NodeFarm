d17c4aa62f0b6b535802d5960844b2099c04e6f3
vivekvag <vivekvag281@gmail.com>
2021-08-02 01:12:03 +0530

    Tweaks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate')
 ////////////////////////////////////
@@ -37,6 +38,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el=>slugify(el.productName, {lower: true}))
+console.log(slugs);
+
 const server = http.createServer((req,res)=>{
     const {query, pathname} = url.parse(req.url,true);
 
@@ -44,7 +48,6 @@ const server = http.createServer((req,res)=>{
     if(pathname === '/' || pathname ==='/overview'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
-        // console.log(cardsHtml);
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
 
